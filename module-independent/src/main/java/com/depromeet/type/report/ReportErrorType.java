61b87a655c9a603ca3884f0c54a07563a01f144e
penrose15 <seongju99@naver.com>
2025-01-03 20:25:26 +0900

    refactor: 탈퇴 사유 ErrorType와 신고 등록 ErrorType 분리 (#469)

--- module-independent/src/main/java/com/depromeet/type/report/ReportErrorType.java
+++ module-independent/src/main/java/com/depromeet/type/report/ReportErrorType.java
@@ -3,7 +3,8 @@ package com.depromeet.type.report;
 import com.depromeet.type.ErrorType;
 
 public enum ReportErrorType implements ErrorType {
-    CANNOT_REPORT_OWN_MEMORY("REPORT_1", "자신의 기록을 신고할 수 없습니다");
+    CANNOT_REPORT_OWN_MEMORY("REPORT_1", "자신의 기록을 신고할 수 없습니다"),
+    FAILED_TO_INSERT_DATA_TO_SPREADSHEET("REPORT_2", "스프레드시트(신고) 업데이트에 실패하였습니다");
 
     private final String code;
     private final String message;
