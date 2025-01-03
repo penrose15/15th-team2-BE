88378d345144656c9a189485c76126295dc3dc3b
penrose15 <seongju99@naver.com>
2025-01-03 20:23:03 +0900

    refactor: 클래스 이름 변경 (ScanningPropertiesConfig -> GoogleSheetConfig) (#469)

--- module-infrastructure/google-spreadsheet/src/main/java/com/depromeet/config/GoogleSheetConfig.java
+++ module-infrastructure/google-spreadsheet/src/main/java/com/depromeet/config/GoogleSheetConfig.java
@@ -4,5 +4,5 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties(value = SpreadSheetProperties.class)
-public class ScanningPropertiesConfig {}
+@EnableConfigurationProperties(value = {SpreadSheetProperties.class, ImageDomainProperties.class})
+public class GoogleSheetConfig {}
